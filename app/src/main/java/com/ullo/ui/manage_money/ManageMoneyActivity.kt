a8c2e7d3e49498b6697589c0487b9424c10f0133
developer <developer@github.io>
2019-07-19 07:28:05 +0530

    Implement Payment method

--- app/src/main/java/com/ullo/ui/manage_money/ManageMoneyActivity.kt
+++ app/src/main/java/com/ullo/ui/manage_money/ManageMoneyActivity.kt
@@ -3,8 +3,14 @@ package com.ullo.ui.manage_money
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import com.flutterwave.raveandroid.RaveConstants
+import com.flutterwave.raveandroid.RavePayActivity
+import com.flutterwave.raveandroid.RavePayManager
+import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.ullo.BR
 import com.ullo.R
@@ -16,6 +22,7 @@ import com.ullo.ui.setting.SettingActivity
 import com.ullo.utils.Validation
 import com.ullo.utils.ViewModelProviderFactory
 import javax.inject.Inject
+
 
 class ManageMoneyActivity : BaseActivity<ActivityManageMoneyBinding, ManageMoneyViewModel>(), ManageMoneyNavigator {
 
@@ -62,9 +69,9 @@ class ManageMoneyActivity : BaseActivity<ActivityManageMoneyBinding, ManageMoney
         mActivityManageMoneyBinding!!.btnUpdate.setOnClickListener {
             if (mActivityManageMoneyBinding!!.btnUpdate.text.toString() == "Add Money") {
                 if (isValid()) {
-                    val req = JsonObject()
-                    req.addProperty("amount", mActivityManageMoneyBinding!!.etAmount.text.toString())
-                    viewModel.userAddMoney(req)
+                    val payableAmount = mActivityManageMoneyBinding!!.etAmount.text.toString().toDouble()
+                    onAddMoney(payableAmount)
+                    /*   */
                 }
             } else {
                 finish()
@@ -87,11 +94,59 @@ class ManageMoneyActivity : BaseActivity<ActivityManageMoneyBinding, ManageMoney
         return bAmount
     }
 
+    private fun onAddMoney(payableAmount: Double) {
+        RavePayManager(this@ManageMoneyActivity).setAmount(payableAmount)
+                .setCountry("GH")
+                .setCurrency("GHS")
+                .setEmail(viewModel.getSession().getAppUser().userData.email)
+                .setfName(viewModel.getSession().getAppUser().userData.fullName)
+                .setlName("")
+                .setNarration("")
+                .setPublicKey("FLWPUBK_TEST-307eea8597a8466c701ee5d9aa67c822-X")
+                .setEncryptionKey("FLWSECK_TESTf18977a09f40")
+                .setTxRef("") //OrderId
+                .acceptAccountPayments(true)
+                .acceptCardPayments(true)
+                .acceptMpesaPayments(true)
+                .acceptGHMobileMoneyPayments(true)
+                .onStagingEnv(false)
+                .allowSaveCardFeature(false)
+                .isPreAuth(false)
+                .initialize()
+    }
+
     override fun onAddMoneySuccess() {
         mActivityManageMoneyBinding!!.imgSuccess.visible()
         mActivityManageMoneyBinding!!.btnUpdate.text = "Close"
         mActivityManageMoneyBinding!!.toolbar.setToolbarTitle("Success")
         mActivityManageMoneyBinding!!.txtContactInfo.text = "Payment Successfully added to your wallet"
         mActivityManageMoneyBinding!!.etAmount.disable()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RaveConstants.RAVE_REQUEST_CODE && data != null) {
+            val message = data.getStringExtra("response")
+            when (resultCode) {
+                RavePayActivity.RESULT_SUCCESS -> {
+                    val gsonBuilder = GsonBuilder()
+                    val gson = gsonBuilder.create()
+                    Log.d("mytag", "message:$message")
+                    /* val newPayment = gson.fromJson(message, Example::class.java)
+                     val txref = String.valueOf(newPayment.getData().getTxRef())
+                     val amount = String.valueOf(newPayment.getData().getAmount())
+                     apiPaymentLink(txref, amount)*/
+                }
+                RavePayActivity.RESULT_ERROR -> Toast.makeText(this, "ERROR $message", Toast.LENGTH_SHORT).show()
+                RavePayActivity.RESULT_CANCELLED -> Toast.makeText(this, "CANCELLED $message", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun apiPaymentLink(txref: String, amount: String) {
+        val req = JsonObject()
+        req.addProperty("amount", amount)
+        req.addProperty("txref", txref)
+        viewModel.userAddMoney(req)
     }
 }
